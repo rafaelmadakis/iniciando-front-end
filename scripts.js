9ddfcb3288e9ad434b83fb72318557af6f893352
rafaelmadakis <rafaelmadakis@gmail.com>
2020-05-16 18:10:47 -0300

    Manupulando a DOM

--- scripts.js
+++ scripts.js
@@ -1,2 +1,13 @@
 const modalOverlay = document.querySelector('.modal-overlay'); // seleciona o modal-overlay
 const cards = document.querySelectorAll('.card'); // selecuiona uma coleção de valores
+
+for (let card of cards) {
+    card.addEventListener("click", function(){
+        modalOverlay.classList.add('active')
+
+    })
+}
+
+document.querySelector(".close-modal").addEventListener("click", function() {
+     modalOverlay.classList.remove("active");
+ })
